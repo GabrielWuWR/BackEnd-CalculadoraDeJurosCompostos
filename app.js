e6bbd80025d80f52c9293592fe66eea9197eb708
GabrielUwUR <gabriel5678uo@gmail.com>
2026-02-11 10:22:05 -0300

    Pequenas correções de erro e comentários adicionados

--- app.js
+++ app.js
@@ -15,12 +15,10 @@ const entradaDeDados = readline.createInterface({
     output: process.stdout
 });
 
+
+//Função para verificar se um numero é realmente válido
 function verificaNumero(num) {
     let numero = num;
-
-    if (typeof numero === 'string') {
-        numero = numero.replace(',', '.');
-    }
 
     let numeroConvertido = Number(numero);
 
@@ -29,8 +27,9 @@ function verificaNumero(num) {
     } else {
         return true;
     }
-}
+};
 
+//Função para tirar % e substituir , por .
 function tratarNumero(num) {
     if (!num) return false;
 
@@ -39,8 +38,9 @@ function tratarNumero(num) {
 
     const numeroConvertido = Number(formatando);
     return numeroConvertido;
-}
+};
 
+//Função para calcular o montante final ela considera se o calculo é em meses ou anos
 function calculaMontante(capitalInicial, taxaJuros, tempoPagamento, tipoPagamento) {
     let c = capitalInicial;
     let i = taxaJuros;
@@ -54,8 +54,9 @@ function calculaMontante(capitalInicial, taxaJuros, tempoPagamento, tipoPagament
         let montante = c * (1 + i) ** n;
         return montante.toFixed(2);
     }
-}
+};
 
+//Função para retornar todos os dados no final
 function mostrarResultado(nomeUser, nomeProduto, valorInicial, vezesParcela, montanteFinal, diferenca) {
     console.log('');
     console.log('******************* [VIVA MODA] *******************');
@@ -66,29 +67,31 @@ function mostrarResultado(nomeUser, nomeProduto, valorInicial, vezesParcela, mon
     console.log(`Muito obrigado por escolher a VIVA MODA.`);
     console.log(`*******************************************************`);
     console.log('');
-}
+};
 
-console.log('******************* VIVA MODA *******************')
-entradaDeDados.question('Digite o nome do cliente: ', (nome) => {
+console.log('');
+console.log('******************* VIVA MODA *******************');
+entradaDeDados.question('Digite o nome do cliente: ', (nome) => { //Pedindo o nome do cliente
     let nomeCliente = nome;
 
-    entradaDeDados.question('Digite o nome do produto: ', (produto) => {
+    entradaDeDados.question('Digite o nome do produto: ', (produto) => { //Pedindo o nome do produto
         let nomeProduto = produto;
 
-        entradaDeDados.question('Insira o valor da compra: ', (compra) => {
+        entradaDeDados.question('Insira o valor da compra: ', (compra) => { //Pedindo o valor da compra
             let valorCompra = tratarNumero(compra);
 
             if (verificaNumero(valorCompra)) {
-                entradaDeDados.question('Insira o valor da taxa de juros: ', (taxa) => {
+                entradaDeDados.question('Insira o valor da taxa de juros: ', (taxa) => { //Pedindo a taxa de juros
                     let valorTaxa = tratarNumero(taxa);
 
-                    if (verificaNumero(valorTaxa)) {
-                        if (valorTaxa > 0) {
+                    if (verificaNumero(valorTaxa)) { //caso o valor da taxa for superior a 1 dividimos por 100
+                        if (valorTaxa > 1) {
                             valorTaxa = valorTaxa / 100;
-                        }
+                        };
 
+                        //O sistema permite o cálculo por meses ou anos
                         entradaDeDados.question('Você quer calcular o tempo em meses(M) ou anos(A): ', (resposta) => {
-                            if (resposta.toLowerCase() == 'm') {
+                            if (resposta.toLowerCase() == 'm') { //Pedindo o tempo em meses
                                 entradaDeDados.question('Insira o tempo de pagamento em MESES: ', (pagamento) => {
                                     let tempoPagamento = pagamento;
 
@@ -96,15 +99,15 @@ entradaDeDados.question('Digite o nome do cliente: ', (nome) => {
                                         let montante = calculaMontante(valorCompra, valorTaxa, tempoPagamento, 'm');
                                         let diferenca = montante - valorCompra;
 
-                                        mostrarResultado(nomeCliente, nomeProduto, valorCompra, tempoPagamento, montante, diferenca);
+                                        mostrarResultado(nomeCliente, nomeProduto, valorCompra.toFixed(2), tempoPagamento, montante, diferenca.toFixed(2));
                                         entradaDeDados.close();
-                                    } else {
+                                    } else { //Caso seja um número inválido ele da um erro
                                         console.log(ERRO);
                                         entradaDeDados.close();
-                                    }
+                                    };
                                 });
 
-                            } else if (resposta.toLowerCase() == 'a') {
+                            } else if (resposta.toLowerCase() == 'a') { //Pedindo o tempo em anos
                                 entradaDeDados.question('Insira o tempo de pagamento em ANOS: ', (pagamento) => {
                                     let tempoPagamento = tratarNumero(pagamento);
 
@@ -112,29 +115,30 @@ entradaDeDados.question('Digite o nome do cliente: ', (nome) => {
                                         let montante = calculaMontante(valorCompra, valorTaxa, tempoPagamento, 'a');
                                         let diferenca = montante - valorCompra;
 
-                                        mostrarResultado(nomeCliente, nomeProduto, valorCompra, tempoPagamento * 12, montante, diferenca);
+                                        //Para o calculo em anos apenas multiplicamos o tempo de pagamento por 12
+                                        mostrarResultado(nomeCliente, nomeProduto, valorCompra.toFixed(2), tempoPagamento * 12, montante, diferenca.toFixed(2));
                                         entradaDeDados.close();
-                                    } else {
+                                    } else { //Caso seja um número inválido ele da um erro
                                         console.log(ERRO);
                                         entradaDeDados.close();
-                                    }
+                                    };
                                 });
-                            } else {
+                            } else { //Caso seja uma resposta inválida ele da um erro
                                 console.log('De uma resposta válida, você pode calcular o tempo em meses(M) ou anos(A)');
                                 entradaDeDados.close();
-                            }
+                            };
                         });
 
-                    } else {
+                    } else { //Caso seja um número inválido ele da um erro
                         console.log(ERRO);
                         entradaDeDados.close();
-                    }
+                    };
                 });
 
-            } else {
+            } else { //Caso seja um número inválido ele da um erro
                 console.log(ERRO);
                 entradaDeDados.close();
-            }
+            };
         });
     });
 });
